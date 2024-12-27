e56f62728779f8f65ffd8428be352cb3a40c5d5f
Elm mafidos <azizelmoujahid2003@gmail.com>
2024-12-27 17:12:34 +0100

    Initial commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        Welcome to Contacts by AbdelAziz El Moujahid
+        <br />
+        Check out my GitHub{" "}
+        <a href="https://github.com/azizelm">
+        GITHUB
+        </a>
+        .
+      </p>
+    );
+  }
+  
